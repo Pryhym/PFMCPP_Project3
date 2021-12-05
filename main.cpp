794f9a7537ac36fa4ef5301717858c35beb41c81
Pryhym <94821015+Pryhym@users.noreply.github.com>
2021-12-05 06:11:42 +0000

    tasks 0-4

--- main.cpp
+++ main.cpp
@@ -107,10 +107,21 @@ struct CarWash
     you should be able to deduce the return type of those functions based on their usage in Person::run()
     You'll need to insert the Person struct from the video in the space below.
  */
-
-
-
-
+   struct Limb
+   {
+       void stepForward();
+       float stepSize();
+   };
+   struct Person
+   {
+       int age;
+       int height;
+       float hairLength;
+       float GPA;
+       unsigned int SATScore;
+       Limb leftFoot;
+       Limb rightFoot;
+   };
 
  /*
  2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
@@ -127,30 +138,14 @@ struct CarWash
 
 
 
-/*
-Thing 1) Laundromat
-5 properties:
-    1) number of washers(int)
-    2) number of dryers(int)
-    3) amount of water used daily(float)
-    4) amount of energy used drying daily(float)
-    5) amount of money made daily(float)
-3 things it can do:
-    1) wash clothes
-    2) dry clothes
-    3) make change out of bills
- */
+
 struct Laundromat
 {
-    //1) number of washers(int)
+    
     int numWashers = 20;
-    //2) number of dryers(int)
-    int numDyers = 20;
-    //3) amount of water used daily(float)
+    int numDyers = 20; 
     float amOfWaterUsedDaily = 200.00f; 
-    //4) amount of energy used drying daily(float)
     float amOfEnergyUsedDaily = 5000.02f;
-    //5) amount of money made daily(float)
     float amOfMoneyMadeDaily = 800.45f;
 
     struct WashingMach
@@ -160,75 +155,79 @@ struct Laundromat
         float waterUsed = 24.57f;
         float powUsed = 75.43f;
         int washes = 17;
-        void washClothes();
+        void wash();
         float countChang(int coins);
         float dwnTime(float timeOff);
     };
 
-    //1) wash clothes
     void washClothes();
-    //2) dry clothes
     void dryClothes();
-    //3) make change out of bills
     float makeChange(float price, float amPaid);
 };
-/*
-Thing 2) bank
-5 properties:
-    1) amount of money deposited into it(float)
-    2) amount of money withdrawn from it(float)
-    3) amount of interest paid(float)
-    4) amount of debt owed(float)
-    5) amount of principal paid(float)
-3 things it can do:
-    1) hold money
-    2) process transactions
-    3) produce a cash balance
- */
+
+void Laundromat::washClothes()
+{
+
+}
+
+void Laundromat::dryClothes()
+{
+
+}
+
+float Laundromat::makeChange(float price, float amPaid)
+{
+    return amPaid - price;
+}
+
+void Laundromat::WashingMach::wash()
+{
+
+}
+
+float Laundromat::WashingMach::countChang(int coins)
+{
+    return coins;
+}
+
+float Laundromat::WashingMach::dwnTime(float timeOff)
+{
+    return timeOff;
+}
+
 struct Bank
 {
-    //1) amount of money deposited into it(float)
-
     float moneyDeposited = 200.25f;
-    //2) amount of money withdrawn from it(float)
     float moneyWithdrawn = 100.25f;
-    //3) amount of interest paid(float)
     float interestPaid = 03.03f;
-    //4) amount of debt owed(float)
     float debtOwed = 2000.05f;
-    //5) amount of principal paid(float)
     float Paid = 1500.25f;
-    //1) hold money
     float holdMoney(float amount);
-    //2) process transactions
     void transaction();
-    //3) produce a cash balance
     float cashBalance(float balance);
 };
-/*
-Thing 3) resturant  
-5 properties:
-    1) number tables available(int)
-    2) number of workers(int)
-    3) number of plates served(int)
-    4) amount of food per family(float)
-    5) number of hours worked per employee(float)
-3 things it can do:
-    1) serve food
-    2) collect payment
-    3) provide eating utensils
- */
+
+float Bank::holdMoney(float amount)
+{
+    return amount;
+}
+
+void Bank::transaction()
+{
+
+}
+
+float Bank::cashBalance(float balance)
+{
+    return balance;
+}
+
 struct Resturant
 {
-    //1) number tables available(int)
     int tblsAvail = 23;
-    //2) number of workers(int)
     int workers = 15;
-    //3) number of plates served(int)
     int platesServed = 42;
-    //4) amount of food per family(float)
     float amFoodPerFaimly = 10.12f;
-    //5) number of hours worked per employee(float)
     float hoursWorked = 39.97f;
 
     struct Server
@@ -243,238 +242,229 @@ struct Resturant
         float countTip(float tip);
     };
 
-    //1) serve food
     void serveFood();
-    //2) collect payment
-    float collectPayement(float payment);
-    //3) provide eating utensils
+    float collectPayment(float payment);
     void utensils();
 };
-/*
-Thing 4) car dealership
-5 properties:
-    1) number of cars in inventory(int)
-    2) amount of cars sold in a month(int)
-    3) amount sales people on staff(int)
-    4) number of cars serviced each week(int)
-    5) amount of inventory bought this month(int)
-3 things it can do:
-    1) sell cars
-    2) hire salesmen
-    3) buy inventory
- */
+
+void Resturant::serveFood()
+{
+
+}
+
+float Resturant::collectPayment(float payment)
+{
+    return payment;
+}
+
+void Resturant::utensils()
+{
+
+}
+
+void Resturant::Server::serveCust()
+{
+
+}
+
+void Resturant::Server::cleanTable()
+{
+
+}
+
+float Resturant::Server::countTip(float tip)
+{
+    return tip;
+}
+
 struct CarDealership
 {
-    //1) number of cars in inventory(int)
     int carInventory = 200;
-    //2) amount of cars sold in a month(int)
     int carSold = 50;
-    //3) amount sales people on staff(int)
     int staff = 23;
-    //4) number of cars serviced each week(int)
     int carServiced = 40;
-    //5) amount of inventory bought this month(int)
     int invBought = 15;
-    //1) sell cars
-    float CarSold(float price);
-    //2) hire salesmen
+    float carSale(float price);
     void salesmen();
-    //3) buy inventory
-    int inventor(int invBought);
+    int invator(int invatory);
 };
-/*
-Thing 5) speakers
-5 properties:
-    1) brand (std::string)
-    2) model number (int)
-    3) size (int)
-    4) frequency range (float)
-    5) impedence (float)
-3 things it can do:
-    1) produce audio
-    2) receive signal
-    3) vibrate
- */
+
+float CarDealership::carSale(float price)
+{
+    return price;
+}
+
+void CarDealership::salesmen()
+{
+
+}
+
+int CarDealership::invator(int invatory)
+{
+    return invatory;
+}
+
 struct Speakers
 {
-    //1) brand (std::string)
-
+ 
     std::string brand = "Vagnavox";
-    //2) model number (int)
     int modelNum = 2340;
-    //3) size (int)
     int size = 12;
-    //4) frequency range (float)
     float freqRange = 145.4f;
-    //5) impedence (float)
     float impedence = 50.0f;
-    //1) produce audio
     void audio();
-    //2) receive signal
     void receiveSignal();
-    //3) vibrate
     void vibrate();
 };
-/*
-Thing 6) amp
-5 properties:
-    1) brand (std::string)
-    2) model number (int)
-    3) power (float)
-    4) impedence (float)
-    5) outputs (int)
-3 things it can do:
-    1) project audio signal
-    2) power speakers
-    3) protect system from failure
- */
+
+void Speakers::audio()
+{
+
+}
+
+void Speakers::receiveSignal()
+{
+    
+}
+
+void Speakers::vibrate()
+{
+    
+}
+
 struct Amp
 {
-    //1) brand (std::string)
     std::string brand = "Fender";
-    //2) model number (int)
     int modelNum = 1200;
-    //3) power (float)
     float power = 200.f;
-    //4) impedence (float)
     float impedence = 200.f;
-    //5) outputs (int)
     int outputs = 4;
-    //1) project audio signal
     void amplify();
-    //2) power speakers
     void speakers();
-    //3) protect system from failure
     void surgeProtect();
 };
-/*
-Thing 7) receiver
-5 properties:
-    1) brand (std::string)
-    2) model number (int)
-    3) functions (int)
-    4) inputs (int)
-    5) outputs (int)
-3 things it can do:
-    1) play cds
-    2) play records
-    3) play radio
- */
+
+void Amp::amplify()
+{
+    
+}
+
+void Amp::speakers()
+{
+    
+}
+
+void Amp::surgeProtect()
+{
+    
+}
+
 struct Receiver
 {
-    //1) brand (std::string)
     std::string brand = "Pioneer";
-    //2) model number (int)
     int modelNum = 2600;
-    //3) functions (int)
     int function = 6;
-    //4) inputs (int)
     int inputs = 5;
-    //5) outputs (int)
     int outputs = 4;
-    //1) play cds
     void playCds();
-    //2) play records
     void playRecords();
-    //3) play radio
     void playRadio();
 };
-/*
-Thing 8) remote
-5 properties:
-    1) brand (std::string)
-    2) model number (int)
-    3) range (float)
-    4) function selector (int)
-    5) batteries needed (int)
-3 things it can do:
-    1) select inputs
-    2) adjust volume
-    3) select mode
- */
+
+void Receiver::playCds()
+{
+    
+}
+
+void Receiver::playRecords()
+{
+    
+}
+
+void Receiver::playRadio()
+{
+    
+}
+
 struct Remote
 {
-    //1) brand (std::string)
     std::string brand = "Pioneer";
-    //2) model number (int)
     int modelNum = 1600;
-    //3) range (float)
     float range = 135.4f;
-    //4) function selector (int)
     int selector = 5;
-    //5) batteries needed (int)
     int batteries = 2;
-    //1) select inputs
     void inputs();
-    //2) adjust volume
     int adjustVol(int volUp, int volDwn, int currVol);
-    //3) select mode
     void mode();
 };
-/*
-Thing 9) turntable
-5 properties:
-    1) brand (std::string)
-    2) model number (int)
-    3) belt speed (float)
-    4) outputs (int)
-    5) needle weight (float)
-3 things it can do:
-    1) plays records
-    2) plays at 33 or 45 rpms
-    3) adjusts for anti-skip
- */
+
+void Remote::inputs()
+{
+    
+}
+
+int Remote::adjustVol(int volUp, int volDwn, int currVol)
+{
+    return currVol += volUp - volDwn;
+}
+
+void Remote::mode()
+{
+    
+}
+
 struct Turntable
 {
-    //1) brand (std::string)
     std::string brand = "Mercury";
-    //2) model number (int)
     int modelNum = 1400;
-    //3) belt speed (float)
     float beltSpeed = 25.4f;
-    //4) outputs (int)
     int outputs = 4;
-    //5) needle weight (float)
     float needleWeight = 0.0043f;
-    //1) plays records
     void playRecord();
-    //2) plays at 33 or 45 rpms
     bool rpms(bool thirtThree);
-    //3) adjusts for anti-skip
     void skipAdjust();
 };
-/*
-Thing 10) stereo system
-5 properties:
-    1) speakers
-    2) amp
-    3) receiver
-    4) remote
-    5) tuntable
-3 things it can do:
-    1) play records
-    2) play radio
-    3) record audio
- */
+
+void Turntable::playRecord()
+{
+    
+}
+
+bool Turntable::rpms(bool thirtThree)
+{
+    return thirtThree;
+}
+
+void Turntable::skipAdjust()
+{
+    
+}
+
 struct StereoSystem
 {
-    //1) speakers
     Speakers speaker;
-    //2) amp
     Amp amp;
-    //3) receiver
     Receiver receiver;
-    //4) remote
     Remote remote;
-    //5) tuntable
     Turntable turntable;
-    //1) play records
     Receiver playRecord();
-    //2) play radio
     Receiver playRadio();
-    //3) record audio
     Receiver recordAudio();
 };
+Receiver StereoSystem::playRecord()
+{
+    return receiver;
+}
+
+Receiver StereoSystem::playRadio()
+{
+    return receiver;
+}
+
+Receiver StereoSystem::recordAudio()
+{
+    return receiver;
+}
 
 /*
  MAKE SURE YOU ARE NOT ON THE MASTER BRANCH
